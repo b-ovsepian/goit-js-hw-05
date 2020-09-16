4e42898bae56e88d50e2434af1491cd0643b50ea
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-09-16 15:27:26 +0300

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -15,21 +15,27 @@ class StringBuilder {
   }
   append(str) {
     this._value += str;
+    return this;
   }
   prepend(str) {
     this._value = str + this._value;
+    return this;
   }
   pad(str) {
-    this._value = str + this._value + str;
+    this.append(str);
+    this.prepend(str);
+    return this;
   }
 }
 const builder = new StringBuilder(".");
 
-builder.append("^");
-console.log(builder.value); // '.^'
+// builder.append("^");
+// console.log(builder.value); // '.^'
 
-builder.prepend("^");
-console.log(builder.value); // '^.^'
+// builder.prepend("^");
+// console.log(builder.value); // '^.^'
 
-builder.pad("=");
-console.log(builder.value); // '=^.^='
+// builder.pad("=");
+// console.log(builder.value); // '=^.^='
+
+console.log(builder.append("^").prepend("^").pad("="));
